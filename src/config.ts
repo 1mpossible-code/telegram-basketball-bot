82495bb61ebb0508cf872d528c0a5d46e221e3ca
Max <linme00p@gmail.com>
2021-08-24 15:37:23 +0300

    Install 'dotenv'. Create config.ts file. Add 'DB_URI' to .env.example

---
+++ src/config.ts
@@ -0,0 +1,20 @@
+// Configure 'dotenv'
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+interface IConfig {
+  token: string | undefined;
+  dbURI: string | undefined;
+}
+
+export const config: IConfig = {
+  token: process.env.BOT_TOKEN,
+  dbURI: process.env.DB_URI
+};
+
+for (const [k, v] of Object.entries(config)) {
+  if (v === undefined) {
+    throw new Error(`${k} must be provided!`);
+  }
+}
