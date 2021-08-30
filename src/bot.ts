e0180db7971bf052e67433b14820eb579a02ec6e
Max <linme00p@gmail.com>
2021-08-30 15:59:29 +0300

    Add 'mongoose' connection to DB into bot.ts.

--- src/bot.ts
+++ src/bot.ts
@@ -1,11 +1,17 @@
 import logger from './Util/logger';
 import Config from './Config';
 import {Telegraf} from 'telegraf';
+import mongoose from 'mongoose';
 
 // Create bot instance
 const bot = new Telegraf(Config.get('BOT_TOKEN'));
 
 bot.start((ctx) => ctx.reply('Hello!'));
+
+mongoose
+    .connect(Config.get('DB_URI'))
+    .then(() => logger.info('Database Connected'))
+    .catch((err) => logger.error(err));
 
 // Launch bot
 bot.launch().then(() => logger.info('Bot is ONLINE'));
