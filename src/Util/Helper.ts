30ca221a7404d1d6a3383bc3ffae19d7faedae59
Max <linme00p@gmail.com>
2021-09-22 21:12:14 +0300

    Add 'botTimeoutMessage' method to Helper.ts utility.

--- src/Util/Helper.ts
+++ src/Util/Helper.ts
@@ -1,1 +1,11 @@
-export class Helper {}
+import {Context} from 'telegraf';
+
+export class Helper {
+    static botTimeoutMessage(
+        ctx: Context,
+        message: string,
+        timeout = 5000
+    ): void {
+        setTimeout(() => ctx.reply(message), timeout);
+    }
+}
