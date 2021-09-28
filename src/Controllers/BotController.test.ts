3d7a5b1140462e25f21b810d275076935d6364fb
Max <linme00p@gmail.com>
2021-09-28 18:02:41 +0300

    Add test for 'singleton' pattern in BotController.test.ts

--- src/Controllers/BotController.test.ts
+++ src/Controllers/BotController.test.ts
@@ -1,4 +1,5 @@
 import {getTestContext} from '../mocks/context';
+import {BotController} from './BotController';
 
 jest.mock('winston');
 
@@ -12,5 +13,11 @@ describe('test BotController', () => {
 
     afterEach(() => {
         jest.resetAllMocks();
+    });
+
+    it('should return the same instance when getInstance() is called', () => {
+        const controller1 = BotController.getInstance();
+        const controller2 = BotController.getInstance();
+        expect(controller1).toEqual(controller2);
     });
 });
