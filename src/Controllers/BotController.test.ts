016d66cb1f366624f2dd12cd7a7a05b15af0b2fe
Max <linme00p@gmail.com>
2021-10-02 14:09:49 +0300

    Add test for changing states in BotController.test.ts

--- src/Controllers/BotController.test.ts
+++ src/Controllers/BotController.test.ts
@@ -1,5 +1,7 @@
 import {getTestContext} from '../Mocks/context';
 import {BotController} from './BotController';
+import {BotPending} from './States/BotPending';
+import {BotState} from './States/BotState';
 
 jest.mock('winston');
 
@@ -16,8 +18,20 @@ describe('test BotController', () => {
     });
 
     it('should return the same instance when getInstance() is called', () => {
-        const controller1 = BotController.getInstance();
-        const controller2 = BotController.getInstance();
+        const controller1: BotController = BotController.getInstance();
+        const controller2: BotController = BotController.getInstance();
         expect(controller1).toEqual(controller2);
+    });
+
+    it('should change state correctly when necessary', () => {
+        // Mock state
+        const state: BotState = new BotPending(BotController.getInstance());
+        state.startCommandHandler = jest.fn();
+
+        // Change state to mocked one
+        BotController.getInstance().changeState(state);
+        BotController.startCommandHandler(ctx);
+
+        expect(state.startCommandHandler).toBeCalledWith(ctx);
     });
 });
