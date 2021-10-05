f4e98a86dab976c6e5818226e3ab80a22a6bc12e
Max <linme00p@gmail.com>
2021-10-05 23:02:04 +0300

    Update State.test.ts structure. Test each state independently.

--- src/Controllers/States/States.test.ts
+++ src/Controllers/States/States.test.ts
@@ -1,21 +1,24 @@
 import {getTestContext} from '../../Mocks/context';
+import {BotEnter} from './BotEnter';
+import {BotController} from '../BotController';
 
 jest.mock('winston');
 
-describe('test states', () => {
-    const ctx = getTestContext();
+const ctx = getTestContext();
 
-    beforeEach(() => {
-        jest.resetModules();
-        ctx.reply = jest.fn().mockResolvedValue(null);
-    });
+beforeEach(() => {
+    jest.resetModules();
+    ctx.reply = jest.fn().mockResolvedValue(null);
+});
 
-    afterEach(() => {
-        jest.resetAllMocks();
-    });
+afterEach(() => {
+    jest.resetAllMocks();
+});
 
-    it('should be example test case', () => {
-        const test = null;
-        expect(test).toBeNull();
+describe('test BotEnter state', () => {
+    it('should test startCommandHandler', () => {
+        const state = new BotEnter(BotController.getInstance());
+        state.startCommandHandler(ctx);
+        expect(ctx.reply).toBeCalledWith('BotEnter');
     });
 });
