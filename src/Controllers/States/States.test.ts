69b0fc84485a02f47610124c4bff56bd4c295dbe
Max <linme00p@gmail.com>
2021-10-07 19:29:29 +0300

    Add tests for BotPending state.

--- src/Controllers/States/States.test.ts
+++ src/Controllers/States/States.test.ts
@@ -2,6 +2,8 @@ import {getTestContext} from '../../Mocks/context';
 import {BotEnter} from './BotEnter';
 import {BotController} from '../BotController';
 import logger from '../../Util/logger';
+import {BotPending} from './BotPending';
+import {PendingService} from '../../Services/PendingService';
 
 jest.mock('winston');
 
@@ -28,6 +30,42 @@ describe('test BotEnter state', () => {
         state.diceHandler(ctx);
         expect(logger.info).toBeCalledWith(
             "BotEnter's 'diceHandler' is called"
+        );
+    });
+
+    it('should test commonMessageHandler method', () => {
+        state.commonMessageHandler(ctx);
+        expect(logger.info).toBeCalledWith('Message handler');
+    });
+
+    it('should test exitCommandHandler method', () => {
+        state.exitCommandHandler(ctx);
+        expect(logger.info).toBeCalledWith('Exit command handler');
+    });
+
+    it('should test callbackQueryHandler method', () => {
+        state.callbackQueryHandler(ctx);
+        expect(logger.info).toBeCalledWith('Callback query handler');
+    });
+});
+
+describe('test BotPending state', () => {
+    const state = new BotPending(BotController.getInstance());
+
+    it('should test startCommandHandler method', () => {
+        state.startCommandHandler(ctx);
+        const markup = PendingService.getStartCommandMarkup();
+
+        expect(ctx.reply).toBeCalledWith(
+            'If you want to play, just click "Join" button',
+            markup
+        );
+    });
+
+    it('should test diceHandler method', () => {
+        state.diceHandler(ctx);
+        expect(logger.info).toBeCalledWith(
+            "BotPending's 'diceHandler' is called"
         );
     });
 
