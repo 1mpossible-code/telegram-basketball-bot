8498963cb96ba7716d7f08dcee5ab88c1e85be54
Max <linme00p@gmail.com>
2021-10-08 13:46:01 +0300

    Cover 100% BotPending.ts with tests.

--- src/Controllers/States/States.test.ts
+++ src/Controllers/States/States.test.ts
@@ -4,6 +4,7 @@ import {BotController} from '../BotController';
 import logger from '../../Util/logger';
 import {BotPending} from './BotPending';
 import {PendingService} from '../../Services/PendingService';
+import {BotGame} from './BotGame';
 
 jest.mock('winston');
 
@@ -74,6 +75,42 @@ describe('test BotPending state', () => {
         expect(logger.info).toBeCalledWith(
             "BotPending's 'diceHandler' is called"
         );
+    });
+
+    it('should test commonMessageHandler method', () => {
+        state.commonMessageHandler(ctx);
+        expect(logger.info).toBeCalledWith('Message handler');
+    });
+
+    it('should test exitCommandHandler method', () => {
+        state.exitCommandHandler(ctx);
+        expect(logger.info).toBeCalledWith('Exit command handler');
+    });
+
+    it('should test callbackQueryHandler method', () => {
+        state.callbackQueryHandler(ctx);
+        expect(logger.info).toBeCalledWith('Callback query handler');
+    });
+});
+
+describe('test BotGame state', () => {
+    const state = new BotGame(BotController.getInstance());
+
+    it('should test startCommandHandler method', () => {
+        state.startCommandHandler(ctx);
+        expect(ctx.reply).toBeCalledWith('BotGame');
+    });
+
+    it('should not throw error when startCommandHandler is called', () => {
+        ctx.reply = jest.fn().mockRejectedValueOnce(Error);
+        state.startCommandHandler(ctx);
+
+        expect(ctx.reply).not.toThrow(Error);
+    });
+
+    it('should test diceHandler method', () => {
+        state.diceHandler(ctx);
+        expect(logger.info).toBeCalledWith("BotGame's 'diceHandler' is called");
     });
 
     it('should test commonMessageHandler method', () => {
