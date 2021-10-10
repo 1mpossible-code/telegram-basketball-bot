569b55b7ea22db262a3157852b845b840d21dc3f
Max <linme00p@gmail.com>
2021-10-10 16:20:42 +0300

    Write tests for BotController.ts to cover 100% of it.

--- src/Controllers/BotController.test.ts
+++ src/Controllers/BotController.test.ts
@@ -5,33 +5,68 @@ import {BotState} from './States/BotState';
 
 jest.mock('winston');
 
+const ctx = getTestContext();
+
+beforeEach(() => {
+    jest.resetModules();
+    ctx.reply = jest.fn().mockResolvedValue(null);
+});
+
+afterEach(() => {
+    jest.resetAllMocks();
+});
+
+it('should return the same instance when getInstance() is called', () => {
+    const controller1: BotController = BotController.getInstance();
+    const controller2: BotController = BotController.getInstance();
+    expect(controller1).toEqual(controller2);
+});
+
 describe('test BotController', () => {
-    const ctx = getTestContext();
+    const mockedState: BotState = new BotPending(BotController.getInstance());
 
-    beforeEach(() => {
-        jest.resetModules();
-        ctx.reply = jest.fn().mockResolvedValue(null);
-    });
-
-    afterEach(() => {
-        jest.resetAllMocks();
-    });
-
-    it('should return the same instance when getInstance() is called', () => {
-        const controller1: BotController = BotController.getInstance();
-        const controller2: BotController = BotController.getInstance();
-        expect(controller1).toEqual(controller2);
-    });
-
-    it('should change state correctly when necessary', () => {
-        // Mock state
-        const state: BotState = new BotPending(BotController.getInstance());
-        state.startCommandHandler = jest.fn();
-
+    it('should change state correctly when necessary and test startCommandHandler method', () => {
         // Change state to mocked one
-        BotController.getInstance().changeState(state);
+        mockedState.startCommandHandler = jest.fn();
+        BotController.getInstance().changeState(mockedState);
         BotController.startCommandHandler(ctx);
 
-        expect(state.startCommandHandler).toBeCalledWith(ctx);
+        expect(mockedState.startCommandHandler).toBeCalledWith(ctx);
+    });
+
+    it('should call state diceHandler', () => {
+        // Change state to mocked one
+        mockedState.diceHandler = jest.fn();
+        BotController.getInstance().changeState(mockedState);
+        BotController.diceHandler(ctx);
+
+        expect(mockedState.diceHandler).toBeCalledWith(ctx);
+    });
+
+    it('should call state commonMessageHandler', () => {
+        // Change state to mocked one
+        mockedState.commonMessageHandler = jest.fn();
+        BotController.getInstance().changeState(mockedState);
+        BotController.commonMessageHandler(ctx);
+
+        expect(mockedState.commonMessageHandler).toBeCalledWith(ctx);
+    });
+
+    it('should call state exitCommandHandler', () => {
+        // Change state to mocked one
+        mockedState.exitCommandHandler = jest.fn();
+        BotController.getInstance().changeState(mockedState);
+        BotController.exitCommandHandler(ctx);
+
+        expect(mockedState.exitCommandHandler).toBeCalledWith(ctx);
+    });
+
+    it('should call state callbackQueryHandler', () => {
+        // Change state to mocked one
+        mockedState.callbackQueryHandler = jest.fn();
+        BotController.getInstance().changeState(mockedState);
+        BotController.callbackQueryHandler(ctx);
+
+        expect(mockedState.callbackQueryHandler).toBeCalledWith(ctx);
     });
 });
